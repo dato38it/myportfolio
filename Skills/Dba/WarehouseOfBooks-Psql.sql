3b6117d90cd107d2209ece3f3418d9361852d71f
dato38it <Dato38it@yandex.ru>
2024-06-01 18:35:11 +0800

    add old projects

---
+++ Skills/Dba/WarehouseOfBooks-Psql.sql
@@ -0,0 +1,97 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.2 (Ubuntu 14.2-1.pgdg20.04+1)
+-- Dumped by pg_dump version 14.2 (Ubuntu 14.2-1.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: book; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.book (
+    book_id integer NOT NULL,
+    title character varying(50),
+    author character varying(30),
+    price numeric(8,2),
+    amount integer
+);
+
+
+ALTER TABLE public.book OWNER TO postgres;
+
+--
+-- Name: book_book_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.book_book_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.book_book_id_seq OWNER TO postgres;
+
+--
+-- Name: book_book_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.book_book_id_seq OWNED BY public.book.book_id;
+
+
+--
+-- Name: book book_id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book ALTER COLUMN book_id SET DEFAULT nextval('public.book_book_id_seq'::regclass);
+
+
+--
+-- Data for Name: book; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.book (book_id, title, author, price, amount) FROM stdin;
+1	Мастер и Маргарита	Булгаков М.А.	670.99	3
+2	Белая гвардия	Булгаков М.А.	540.50	5
+3	Идиот	Достоевский Ф.М.	460.00	10
+4	Братья Карамазовы	Достоевский Ф.М.	799.01	2
+\.
+
+
+--
+-- Name: book_book_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.book_book_id_seq', 4, true);
+
+
+--
+-- Name: book book_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.book
+    ADD CONSTRAINT book_pkey PRIMARY KEY (book_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
